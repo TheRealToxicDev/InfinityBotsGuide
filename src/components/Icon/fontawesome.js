2cf92cd4f966eba1d78f4349e4faf3fe8641cfc2
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2020-11-09 18:25:42 -0700

    Add files via upload

---
+++ src/components/Icon/fontawesome.js
@@ -0,0 +1,17 @@
+export {
+  faInfoCircle,
+  faChevronRight,
+  faLink,
+  faPencilAlt,
+  faTimes,
+  faBars,
+  faExclamationCircle,
+  faExclamationTriangle,
+  faCheckCircle,
+  faArrowRight,
+  faLock,
+  faMoon,
+  faSun,
+  faSearch
+} from "@fortawesome/free-solid-svg-icons";
+export { faGithub, faDiscord } from "@fortawesome/free-brands-svg-icons";
